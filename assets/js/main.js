12eb8c20d43f73c584edc687e46846d95bd6f12d
integra-web <contact@integra-web.net>
2022-06-15 18:22:07 +0200

    Responsive Tablette

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    //variable for the 'stroke-dashoffset' unit
-    var $dashOffset = $(".path").css("stroke-dashoffset");
-    //on a scroll event - execute function
-    $(window).scroll(function() {
-      //calculate how far down the page the user is 
-      var $percentageComplete = (($(window).scrollTop() / ($("html").height() - $(window).height())) * 100);
-      //convert dashoffset pixel value to interger
-      var $newUnit = parseInt($dashOffset, 10);
-      //get the value to be subtracted from the 'stroke-dashoffset'
-      var $offsetUnit = $percentageComplete * ($newUnit / 100);
-      //set the new value of the dashoffset to create the drawing effect
-      $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);
-      
-  });
+
+  var myline = document.getElementById("myline");
+  var length = myline.getTotalLength();
+  myline.style.strokeDasharray = length;
+  
+  myline.style.strokeDashoffset = length;
+  
+  window.addEventListener("scroll", myFunction);
+  
+  function myFunction() {
+    var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    var draw = length * scrollpercent;
+  
+    myline.style.strokeDashoffset = length - draw;
+  
+    endPoint = myline.getPointAtLength(draw);
+  
+  }
+
 });
